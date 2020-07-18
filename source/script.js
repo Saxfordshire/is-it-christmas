5a376f08b1c8a023af2c2f808143fd053a99cd14
Saxfordshire <66988024+Saxfordshire@users.noreply.github.com>
2020-07-18 13:08:15 -0700

    Delete script.js

--- source/script.js
+++
@@ -1,7 +0,0 @@
-var date = new Date();
-
-if (date.getMonth() == 11 && date.getDate() == 25){
-    document.getElementById("text").innerHTML = "<p>Yes.</p>";
-} else {
-    document.getElementById("text").innerHTML = "<p>No.</p>";
-}
